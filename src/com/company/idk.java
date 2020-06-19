f9da9c204012ab61fe374dcec6afbbcdb22e70f4
Vasyl Hlovliuk <Vasuahp16@gmail.com>
2020-06-19 19:23:18 +0300

    b

--- src/com/company/idk.java
+++ src/com/company/idk.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class idk {
 
-    public static void idk (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Vasyl Hlovliuk");
     }
 }
