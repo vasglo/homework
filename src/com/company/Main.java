4f4f7cddb0c4f9c87db77974c9197c3d74f5a8ec
Vasyl Hlovliuk <Vasuahp16@gmail.com>
2020-06-19 17:11:16 +0300

    first homework

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("1.Прокинутися");
+        System.out.println("2.Почистити зуби");
+        System.out.println("3.Спробувати захопити владу у всесвіті");
+        System.out.println("4.Повторити завтра, починаючи з пункту №1");
+    }
+}
---
+++ src/com/company/Myname.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Myname {
+    public static void myname(String[] args) {
+        System.out.println("Василь Гловлюк");
+    }
+}
